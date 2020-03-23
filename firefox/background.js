d16566e1088e86bddbb09f566be47624cc0b583d
Wesley Branton <addons@wesleybranton.com>
2020-03-22 23:50:10 -0400

    Code beautify

--- firefox/background.js
+++ firefox/background.js
@@ -2,47 +2,62 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
-// Create block handler
+/**
+ * Create block listener
+ * @async
+ */
 async function createBlocker() {
-	// Remove previous listener
-	browser.webRequest.onBeforeRequest.removeListener(block);
-	
-	// Load URLs from storage
-	let urls = await browser.storage.sync.get();
-	
-	// Check if there are URLs to load
-	if (urls.urlList) {
-		// Create URL fliter list
-		filter = [];
-		for (i = 0; i < urls.urlList.length; i++) {
-			filter.push(urls.urlList[i]);
-		}
-		
-		// Create listener
-		browser.webRequest.onBeforeRequest.addListener(block, {urls: filter}, ["blocking"]);
-	}
+    // Remove previous listener
+    browser.webRequest.onBeforeRequest.removeListener(block);
+
+    // Load URLs from storage
+    let urls = await browser.storage.sync.get();
+
+    // Check if there are URLs to load
+    if (urls.urlList) {
+        // Create URL fliter list
+        filter = [];
+        for (i = 0; i < urls.urlList.length; i++) {
+            filter.push(urls.urlList[i]);
+        }
+
+        // Create listener
+        browser.webRequest.onBeforeRequest.addListener(block, {
+            urls: filter
+        }, ["blocking"]);
+    }
 }
 
-// Handle blocked URL
+/**
+ * Handle blocked URL
+ * @param {Object} requestDetails
+ */
 function block(requestDetails) {
-	return {redirectUrl: browser.runtime.getURL('/blocked/blockpage.html')};
+    return {
+        redirectUrl: browser.runtime.getURL('/blocked/blockpage.html')
+    };
 }
 
-// Handles missing data
+/**
+ * Handles missing storage data
+ * @async
+ */
 async function checkData() {
-	// Load URLs from storage
-	let data = await browser.storage.sync.get();
-	
-	// Create blank URL list in storage if required
-	if (!data.urlList) {
-		browser.storage.sync.set({urlList:[]});
-	}
+    // Load URLs from storage
+    let data = await browser.storage.sync.get();
+
+    // Create blank URL list in storage if required
+    if (!data.urlList) {
+        browser.storage.sync.set({
+            urlList: []
+        });
+    }
 }
 
-var filter = [];
+let filter = [];
 createBlocker();
 browser.storage.onChanged.addListener(createBlocker);
 browser.browserAction.onClicked.addListener(() => {
-	browser.runtime.openOptionsPage()
+    browser.runtime.openOptionsPage()
 });
 checkData();
