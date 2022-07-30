baf3e6c774e144148c9ef2589eb005040674f240
Wesley Branton <addons@wesleybranton.com>
2022-07-29 23:51:50 -0400

    Added uninstall survey (#35)

--- src/background.js
+++ src/background.js
@@ -66,6 +66,30 @@ function handleInstalled(details) {
     }
 }
 
+/**
+ * Set up uninstall page
+ */
+ function setUninstallPage() {
+    getSystemDetails((details) => {
+        browser.runtime.setUninstallURL(`${webBase}/uninstall/?browser=${details.browser}&os=${details.os}&version=${details.version}`);
+    });
+}
+
+/**
+ * Send system details to callback
+ * @param {Function} callback
+ */
+function getSystemDetails(callback) {
+    browser.runtime.getPlatformInfo((platform) => {
+        callback({
+            browser: 'firefox',
+            version: browser.runtime.getManifest().version,
+            os: platform.os
+        });
+    });
+}
+
+const webBase = 'https://addons.wesleybranton.com/addon/website-blocker';
 let filter = [];
 let showError = true;
 createBlocker();
@@ -75,3 +99,4 @@ browser.browserAction.onClicked.addListener(() => {
     browser.runtime.openOptionsPage();
 });
 checkData();
+setUninstallPage();
